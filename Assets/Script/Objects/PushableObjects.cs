eddb7c69462b983032dcb5e54b266ba3720194ec
Parveen-Rai <47239629+Parveen-Rai@users.noreply.github.com>
2026-05-18 21:48:29 +0530

    block collision

--- Assets/Script/Objects/PushableObjects.cs
+++ Assets/Script/Objects/PushableObjects.cs
@@ -5,17 +5,34 @@ public class PushableObjects : KinematicBody
 {
     public Collider2D objectBottomCollider;
     public LayerMask groundCollisionLayer;
+
+    public float pushResistance = 1f;
     private float Gravity = -20f;
 
     private float fallMultiplier = 2.5f;
+
+    private bool isPushed = false;
 
  
     void Update()
     {
         CheckforGround(objectBottomCollider, 0f, groundCollisionLayer, 0.1f);
-        snapToGround(1f);
+        snapToGround(0.8f);
         ApplyGravity(Gravity, fallMultiplier);
         Move();
+    }
+
+    public void push(float horizontalPush)
+    {
+        isPushed = horizontalPush != 0f;
+        if(isPushed)
+        {
+            horizontalVelocity = horizontalPush / pushResistance;
+        }
+        else
+        {
+            horizontalVelocity = 0f;
+        }
     }
 
     override protected void Move()
@@ -35,7 +52,7 @@ public class PushableObjects : KinematicBody
             // float moveX = horizontalVelocity * Time.deltaTime;
 
             // moveX = ResolveHorizontalCollisions(moveX);
-                float moveX = 0f;
+                float moveX = horizontalVelocity * Time.deltaTime;
 
             totalVelocity = new Vector3(
                 moveX / Time.deltaTime,
